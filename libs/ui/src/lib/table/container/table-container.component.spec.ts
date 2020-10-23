1be9f1794b769caf6f7d8f8c716e02116730d9ae
kanazawanao <naomiti3333@gmail.com>
2020-10-24 00:36:42 +0900

    uiテスト実装

--- libs/ui/src/lib/table/container/table-container.component.spec.ts
+++ libs/ui/src/lib/table/container/table-container.component.spec.ts
@@ -1,6 +1,17 @@
+import { TableContainerComponent } from './table-container.component';
+import { TablePresenterInputData } from '../presenter/table-presenter-input-data';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableContainerComponent } from './table-container.component';
+/** プレゼンタコンポーネントのモック */
+@Component({
+  selector: 'ui-table-presenter',
+  template: '',
+  styleUrls: [],
+})
+export class MockTalbePresenterComponent {
+  @Input() inputData: TablePresenterInputData;
+}
 
 describe('TableContainerComponent', () => {
   let component: TableContainerComponent;
@@ -8,9 +19,8 @@ describe('TableContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TableContainerComponent ]
-    })
-    .compileComponents();
+      declarations: [TableContainerComponent, MockTalbePresenterComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
