f4f03dca166c6bfe8f39265660746409d7d9091e
kanazawanao <naomiti3333@gmail.com>
2020-10-25 19:07:18 +0900

    微修正

--- apps/client/src/app/pages/skill-info/presenter/skill-info-presenter.component.ts
+++ apps/client/src/app/pages/skill-info/presenter/skill-info-presenter.component.ts
@@ -6,15 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-info-presenter.component.scss'],
 })
 export class SkillInfoPresenterComponent implements OnInit {
-  columns = ['name', 'years', 'skillLevel'];
+  columns = ['name', 'experienceYears', 'skillLevel'];
 
   data = [
-    { name: 'JavaScript', years: 1, skillLevel: 2 },
-    { name: 'JQuery', years: 1, skillLevel: 1 },
-    { name: 'TypeScript', years: 1, skillLevel: 4 },
-    { name: 'C#', years: 2, skillLevel: 4 },
-    { name: 'Java 7', years: 1, skillLevel: 3 },
-    { name: 'GAS', years: 1, skillLevel: 3 },
+    { name: 'JavaScript', experienceYears: 1, skillLevel: 2 },
+    { name: 'JQuery', experienceYears: 1, skillLevel: 1 },
+    { name: 'TypeScript', yeaexperienceYearsrs: 1, skillLevel: 4 },
+    { name: 'C#', experienceYears: 2, skillLevel: 4 },
+    { name: 'Java 7', experienceYears: 1, skillLevel: 3 },
+    { name: 'GAS', experienceYears: 1, skillLevel: 3 },
   ];
   constructor() {}
 
