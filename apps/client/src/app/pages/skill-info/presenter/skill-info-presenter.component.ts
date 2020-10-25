e246dc29b77b2eaab4e81a93a71634396e7bbf01
kanazawanao <naomiti3333@gmail.com>
2020-10-25 21:00:24 +0900

    誤記修正

--- apps/client/src/app/pages/skill-info/presenter/skill-info-presenter.component.ts
+++ apps/client/src/app/pages/skill-info/presenter/skill-info-presenter.component.ts
@@ -11,7 +11,7 @@ export class SkillInfoPresenterComponent implements OnInit {
   data = [
     { name: 'JavaScript', experienceYears: 1, skillLevel: 2 },
     { name: 'JQuery', experienceYears: 1, skillLevel: 1 },
-    { name: 'TypeScript', yeaexperienceYearsrs: 1, skillLevel: 4 },
+    { name: 'TypeScript', experienceYears: 1, skillLevel: 4 },
     { name: 'C#', experienceYears: 2, skillLevel: 4 },
     { name: 'Java 7', experienceYears: 1, skillLevel: 3 },
     { name: 'GAS', experienceYears: 1, skillLevel: 3 },
