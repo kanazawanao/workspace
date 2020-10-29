432f6469a07d4e72e669a2137d4008f39a31c9f6
kanazawanao <naomiti3333@gmail.com>
2020-10-29 19:25:40 +0900

    テスト修正

--- libs/ui/src/lib/select/container/select-container.component.spec.ts
+++ libs/ui/src/lib/select/container/select-container.component.spec.ts
@@ -1,8 +1,11 @@
 import { SelectContainerComponent } from './select-container.component';
 import { SelectPresenterInputData } from '../presenter/select-presenter-input-data';
 import { UiSelectModule } from '../ui-select.module';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /** プレゼンタコンポーネントのモック */
@@ -22,7 +25,12 @@ describe('SelectContainerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SelectContainerComponent, MockSelectPresenterComponent],
-      imports: [UiSelectModule, BrowserAnimationsModule],
+      imports: [
+        CommonModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule,
+      ],
     }).compileComponents();
   });
 
