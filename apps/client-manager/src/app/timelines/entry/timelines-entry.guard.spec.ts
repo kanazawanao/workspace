dfb3483d295d7d46712cb74c74d20908b7272393
kanazawanao <naomiti3333@gmail.com>
2020-11-22 08:06:22 +0900

    test実装

--- apps/client-manager/src/app/timelines/entry/timelines-entry.guard.spec.ts
+++ apps/client-manager/src/app/timelines/entry/timelines-entry.guard.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed } from '@angular/core/testing';
-
 import { TimelinesEntryGuard } from './timelines-entry.guard';
+import { TestBed } from '@angular/core/testing';
+import { MockTimelinesFacade } from '../+state/mock-timelines.facade';
+import { TimelinesFacade } from '../+state/timelines.facade';
 
 describe('TimelinesEntryGuard', () => {
   let guard: TimelinesEntryGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TimelinesFacade,
+          useValue: MockTimelinesFacade,
+        },
+      ],
+    });
     guard = TestBed.inject(TimelinesEntryGuard);
   });
 
