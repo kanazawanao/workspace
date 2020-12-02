4183eed4d79880aae413b98d433dd0baa5b1b848
kanazawanao <naomiti3333@gmail.com>
2020-12-02 21:39:04 +0900

    users-data-accessモジュールの修正

--- libs/client-manager/users/src/lib/data-access/users-data-access.module.ts
+++ libs/client-manager/users/src/lib/data-access/users-data-access.module.ts
@@ -1,8 +1,19 @@
+import { UsersDataAccessService } from './users-data-access.service';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { UsersFacade } from './+state/users.facade';
+import { UsersEffects } from './+state/users.effects';
+import * as fromUsers from './+state/users.reducer';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
+    EffectsModule.forFeature([UsersEffects]),
+  ],
+  providers: [UsersFacade, UsersDataAccessService],
 })
 export class UsersDataAccessModule {}
