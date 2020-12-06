4e3eec7c0fdd9da17c583bca89b24bfca5e4f080
kanazawanao <naomiti3333@gmail.com>
2020-12-06 23:29:08 +0900

    修正

--- libs/client-manager/login/src/lib/login.service.ts
+++ libs/client-manager/login/src/lib/login.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { ILogin, IToken } from '@workspace/api-interfaces';
+import { ApiConstant } from '@workspace/constants';
+import { HttpRequestService } from '@workspace/shared-service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import {
   FormBuilder,
   FormControl,
@@ -8,7 +13,12 @@ import {
 
 @Injectable()
 export class LoginService {
-  constructor(private formBuilder: FormBuilder) {}
+  constants = ApiConstant;
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpRequestService: HttpRequestService,
+    @Inject('apiUrl') private apiUrl: string
+  ) {}
 
   generateFormGroup(): FormGroup {
     const formGroup = this.formBuilder.group({
@@ -19,5 +29,20 @@ export class LoginService {
       ]),
     });
     return formGroup;
+  }
+
+  login(login: ILogin): Observable<IToken> {
+    const res = this.httpRequestService
+      .post<IToken>({
+        url: `${this.apiUrl}${this.constants.login}`,
+        body: login,
+      })
+      .pipe(
+        map((x) => {
+          this.httpRequestService.token = x.accessToken;
+          return x;
+        })
+      );
+    return res;
   }
 }
